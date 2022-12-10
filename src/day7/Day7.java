038bc2877bda015490d1e39ef8f4e14199efd9db
maxmilliam.marchesini <m.marchesini@reply.it>
2022-12-10 14:08:14 +0100

    fix string inputTest

--- src/day7/Day7.java
+++ src/day7/Day7.java
@@ -5,29 +5,31 @@ public class Day7 {
 
     public static void main(String[] args) {
 
-        String input = "$ cd /\n" +
-                "$ ls\n" +
-                "dir a\n" +
-                "14848514 b.txt\n" +
-                "8504156 c.dat\n" +
-                "dir d\n" +
-                "$ cd a\n" +
-                "$ ls\n" +
-                "dir e\n" +
-                "29116 f\n" +
-                "2557 g\n" +
-                "62596 h.lst\n" +
-                "$ cd e\n" +
-                "$ ls\n" +
-                "584 i\n" +
-                "$ cd ..\n" +
-                "$ cd ..\n" +
-                "$ cd d\n" +
-                "$ ls\n" +
-                "4060174 j\n" +
-                "8033020 d.log\n" +
-                "5626152 d.ext\n" +
-                "7214296 k";
+        String input = """
+$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k
+        """;
 
         Cartella root = new Cartella();
         root.nome = "/";
